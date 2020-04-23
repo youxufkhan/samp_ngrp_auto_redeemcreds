6066d3b631e06bbe276f432c744cb90d5ff1f01b
youxufkhan <m.youxuf@gmail.com>
2020-04-23 18:11:44 +0500

    added check so script only executes command when new valid code is recieved script now runs every 3 minutes screenshot delay due to lag

--- index.js
+++ index.js
@@ -17,18 +17,24 @@ setInterval(() => {
         // data using tostring function.
         console.log("Matching")
         var matches = data.match(/\/redeemcreds .{10}/g)
-        console.log(matches?matches.length:'0' + " matches found");
+        console.log(matches ? matches.length : '0' + " matches found");
         if (matches) {
-            fs.writeFile('code.txt', matches[matches.length - 1], (err) => {
-                // In case of a error throw err. 
-                if (err) throw err;
-                console.log("Write to file complete")
-                console.log("Executing AHK")
-                exec('redeemcredits.exe', function (err, data) {
-                    if (err) { console.log(err) }
-                    else { console.log("Success"); }
-                });
+            fs.readFile('code.txt', 'utf-8', (err, code) => {
+                if (code != matches[matches.length - 1]) {
+                    fs.writeFile('code.txt', matches[matches.length - 1], (err) => {
+                        // In case of a error throw err. 
+                        if (err) throw err;
+                        console.log("Write to file complete")
+                        console.log("Executing AHK")
+                        exec('redeemcredits.exe', function (err, data2) {
+                            if (err) { console.log(err) }
+                            else { console.log("Success"); }
+                        });
+                    })
+                }else{
+                    console.log('no new code')
+                }
             })
         }
     })
-}, 3000)
+}, 180000)
--- redeemcredits.ahk
+++ redeemcredits.ahk
@@ -2,6 +2,7 @@ FileRead, Contents, code.txt
 if not ErrorLevel  ; Successfully loaded.
 {
 	   	SendInput, t%Contents%{enter}
+		Sleep 3000 
 		Send {F8}
 }
 
